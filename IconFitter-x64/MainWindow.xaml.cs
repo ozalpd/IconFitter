402702b881e05a952ac347591c73f80eb7e3d026
Özalp Döndüren <ozalp@donduren.com>
2019-09-08 13:51:06 +0300

    IconFitter-x64 became unaware of Magick.NET x64

--- IconFitter-x64/MainWindow.xaml.cs
+++ IconFitter-x64/MainWindow.xaml.cs
@@ -1,21 +1,7 @@
 ﻿using IconLib.Models;
-using ImageMagick;
 using IconLib.ViewModels;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Reflection;
 
 namespace IconFitter
