682ccf55b6bc20247ca737c5b69c6f0c4af6bbe4
Özalp Döndüren <ozalp@donduren.com>
2020-05-08 00:52:19 +0300

    Added TextBoxSelectAll a derıved TextBox with behavior SelectAllOnFocus

---
+++ IconFitter-x64/Behaviors/TextBoxSelectAll.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Windows.Controls;
+
+namespace IconFitter.Behaviors
+{
+    /// <summary>
+    /// TextBox with behavior SelectAllOnFocus
+    /// </summary>
+    public class TextBoxSelectAll : TextBox
+    {
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+            var b = Interaction.GetBehaviors(this);
+            b.Add(new SelectAllOnFocus());
+        }
+    }
+}
